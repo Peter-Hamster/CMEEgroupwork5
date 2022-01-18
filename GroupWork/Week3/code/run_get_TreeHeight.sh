586e025a1cd013d97760e562659e3d781426c546
Kate <keg121@ic.ac.uk>
2022-01-18 14:57:11 +0000

    Group Work

---
+++ GroupWork/Week3/code/run_get_TreeHeight.sh
@@ -0,0 +1,10 @@
+#########################################################################
+# Shell script which tests get_TreeHeight.R using trees.csv as an example
+##########################################################################
+
+#! /bin/sh
+ipython3 get_TreeHeight.py trees
+
+Rscript get_TreeHeight.R trees
+
+
